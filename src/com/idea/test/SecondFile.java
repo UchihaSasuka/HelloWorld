c2f288b040ad44f7951c4f6e65d27ffba8920a64
li peng <303013435@qq.com>
2017-07-11 14:52:26 +0800

    Test file Add

--- src/com/idea/test/SecondFile.java
+++ src/com/idea/test/SecondFile.java
@@ -6,5 +6,6 @@ package com.idea.test;
 public class SecondFile {
     public static void main(String[] args){
         System.out.println("这是第一个测试文件");
+        System.out.println("hadadad");
     }
 }
