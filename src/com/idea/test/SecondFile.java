4546c9d1533bda3f7b363ee5740cf9c5826fb277
li peng <303013435@qq.com>
2017-07-11 14:50:29 +0800

    Test file Add

---
+++ src/com/idea/test/SecondFile.java
@@ -0,0 +1,10 @@
+package com.idea.test;
+
+/**
+ * Created by zjl on 2017/7/11.
+ */
+public class SecondFile {
+    public static void main(String[] args){
+        System.out.println("这是第一个测试文件");
+    }
+}
